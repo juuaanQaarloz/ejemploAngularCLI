e67cf5b93fdd97ab03bb67885b33934dcada71c2
odalysmarron <odalys.marron@solinte.com.mx>
2019-12-11 09:40:10 -0600

    add message input to error banner component

--- src/app/components/error-banner/error-banner.component.ts
+++ src/app/components/error-banner/error-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-error-banner',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorBannerComponent implements OnInit {
   showBanner = true;
+  @Input() message;
 
   constructor() { }
 
