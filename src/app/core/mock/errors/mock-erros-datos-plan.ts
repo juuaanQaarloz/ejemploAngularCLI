7a8714a5b25077d20d0f6d3e87130d9c5461fd63
odalysmarron <odalys.marron@solinte.com.mx>
2019-12-11 16:33:41 -0600

    correct error messages

--- src/app/core/mock/errors/mock-erros-datos-plan.ts
+++ src/app/core/mock/errors/mock-erros-datos-plan.ts
@@ -7,10 +7,10 @@ export const ERROSDATOSPLAN: Error[] = [
   },
   {
     errorName: 'invalidAssuredImportMxn',
-    errorMsg: 'Cantidad mínima para la suma asegurada es de $400,000 pesos'
+    errorMsg: 'Cantidad mínima para la suma asegurada debe ser de $400,000 pesos'
   },
   {
     errorName: 'invalidAssuredImportUsd',
-    errorMsg: 'Cantidad mínima para la suma asegurada es de $40,000 dolares'
+    errorMsg: 'Cantidad mínima para la suma asegurada debe ser de $40,000 dolares'
   }
 ];
