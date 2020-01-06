c58855aaeb4c09b826626023f764240a4f466c0a
Javier Pantaleón D <xavierpanta@gmail.com>
2020-01-06 04:51:12 -0600

    Se modifica URL de servicios.

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,6 +2,5 @@
  * appConstants, aqui se definen todas las constantes que se utilizaran en la aplicación.
  */
 export class AppConstants {
-  //public static readonly URL_SERVICE: string = 'http://localhost:9080/despriv';
-  public static readonly URL_SERVICE: string = 'http://localhost:8080/CP-DES_PRIV_SVS_6979-0.0.1';
+  public static readonly URL_SERVICE: string = 'http://localhost:9080/despriv';
 }
