2a6e7384593284a092aedec279d2f18fd4d31f75
Israel Esteban Gomez Lopez <israel-esteban.gomez-lopez@metlife.com.mx>
2020-01-07 18:06:42 -0600

    Se corrigen faltas de ortografia

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,6 +2,6 @@
  * appConstants, aqui se definen todas las constantes que se utilizaran en la aplicación.
  */
 export class AppConstants {
-  public static readonly URL_SERVICE: string = 'http://localhost:9080/despriv';
-  public static readonly URL_SERVICE_DEV: string = 'http://10.215.104.61:35741/cp-desws-priv';
+  public static readonly URL_SERVICE: string = 'http://localhost:9090/despriv';
+  public static readonly URL_SERVICE_DEV: string = 'http://localhost:9090/despriv';
 }
