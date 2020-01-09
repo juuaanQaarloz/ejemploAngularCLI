be1ec831f1fc90b2175bc732270cfae0ae57761a
odalysmarron <odalys.marron@solinte.com.mx>
2020-01-09 17:53:17 -0600

    add agents, beneficiaries and diseases map and save functionality

--- src/app/models/applicationJson/agentJson/agentJson.ts
+++ src/app/models/applicationJson/agentJson/agentJson.ts
@@ -3,11 +3,12 @@ import {AgentCd} from './agentCd';
 export class AgentJson {
   app_id?: number = 0;
   agnt_cd?: AgentCd = new AgentCd();
+  agnt_fmt_2_id?: number = 0;
   agnt_part_per?: number = 0;
   part_ord?: number = 0;
   rec_crt_ts?: string = null;
-  rec_crt_usr_id?: string = null;
+  rec_crt_usr_id?: string = 'string';
   rec_updt_ts?: string = null;
-  rec_updt_usr_id?: string = null;
-  agnt_fmt_2_id?: number = 0;
+  rec_updt_usr_id?: string = 'string';
+
 }
