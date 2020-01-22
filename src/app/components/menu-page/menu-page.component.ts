8ee663f1ae6a3fdd82deb939b16e2bcd7b820011
Javier Pantaleón D <xavierpanta@gmail.com>
2020-01-22 00:52:22 -0600

    Se modifica funcionalidad para obtener parámetros metrolename, metroluid.

--- src/app/components/menu-page/menu-page.component.ts
+++ src/app/components/menu-page/menu-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { AppConstants } from 'src/app/app.constants';
 
@@ -12,6 +12,7 @@ import { AppConstants } from 'src/app/app.constants';
 export class MenuPageComponent implements OnInit {
   metrolename:string;
   metroluid:string;
+  token:string;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -26,29 +27,28 @@ export class MenuPageComponent implements OnInit {
     };
     localStorage.setItem( 'sessionUser', JSON.stringify(sessionUser));
 
-    this.httpClient.get( AppConstants.URL_SERVICE_DEV  + '/getUserData').subscribe((resp:any) => {
-      localStorage.setItem("metrolename", resp.data.metrolename);
-      localStorage.setItem("metroluid", resp.data.metUserId);
-      localStorage.setItem("token", resp.data.temporalToken);
-      localStorage.setItem("userId", '1120');
-
-      console.log("metrolename: "+ localStorage.getItem("metrolename") );
-      console.log("metroluid: "+ localStorage.getItem("metroluid") );
-      console.log("token: "+ localStorage.getItem("token") );
-      console.log("userId: "+ localStorage.getItem("userId") );
-    });
-/*
     this.activatedRoute.queryParams.subscribe( params => {
       this.metrolename = params['metrolename'];
       this.metroluid = params['metroluid'];
 
+      if(this.metrolename===undefined || this.metroluid===undefined){
+        console.log("entra al servicio getUserData");
+        this.httpClient.get( AppConstants.URL_SERVICE_DEV  + '/getUserData').subscribe((resp:any) => {
+          this.metrolename = resp.data.metrolename;
+          this.metroluid = resp.data.metUserId;
+          this.token = resp.data.temporalToken;
+          localStorage.setItem("token", this.token); 
+        });
+      }
+
       localStorage.setItem("metrolename", this.metrolename);
       localStorage.setItem("metroluid", this.metroluid);
       localStorage.setItem("userId", '1120');
-      //console.log("metrolename: "+this.metrolename);
-      //console.log("metroluid: "+this.metroluid);
-    });
-*/
-  }
 
+      console.log("metrolename: "+localStorage.getItem('metrolename'));
+      console.log("metroluid: "+localStorage.getItem('metroluid'));
+      console.log("token: "+localStorage.getItem('token'));
+      console.log("userId: "+localStorage.getItem('userId'));
+    });
+  }
 }
