ca3004545cab81cfa464edea239b5cf6797e1f0e
Israel Esteban Gomez Lopez <israel-esteban.gomez-lopez@metlife.com.mx>
2020-01-23 11:56:49 -0600

    Se invoca servicio de token

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,15 +2,18 @@
  * appConstants, aqui se definen todas las constantes que se utilizaran en la aplicación.
  */
 export class AppConstants {
-  public static readonly URL_SERVICE: string = 'http://localhost:9090/despriv';
-  public static readonly URL_SERVICE_DEV: string = 'http://localhost:9090/despriv';
+  //public static readonly URL_SERVICE: string = 'http://localhost:9090/despriv';
+  //public static readonly URL_SERVICE_DEV: string = 'http://localhost:9090/despriv';
   // public static readonly URL_SERVICE: string = 'https://dev.des.metlife.com/despriv';
   
   // public static readonly URL_SERVICE_DEV: string = 'http://10.215.104.61:35741/cp-desws-priv';
   
   
-  //public static readonly URL_SERVICE: string  = 'https://dev.des.metlife.com/despriv';
-  //public static readonly URL_SERVICE_DEV: string = 'https://dev.des.metlife.com/despriv';
+  public static readonly URL_SERVICE: string  = 'https://dev.des.metlife.com/despriv';
+  public static readonly URL_SERVICE_DEV: string = 'https://dev.des.metlife.com/despriv';
+
 
   public static readonly URL_BROKER_SERVICE: string = 'http://10.184.10.166:8585/esb/appSerPriv/api/v1/app';
 }
+
+
