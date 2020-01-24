3e177564ea47eb0679a0f175e6b30d66215a9f74
odalysmarron <odalys.marron@solinte.com.mx>
2020-01-23 18:49:26 -0600

    update services url

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,11 +2,11 @@
  * appConstants, aqui se definen todas las constantes que se utilizaran en la aplicación.
  */
 export class AppConstants {
-  public static readonly URL_SERVICE: string = 'https://dev.des.metlife.com/despriv';
+  public static readonly URL_SERVICE: string = 'https://int.des.metlife.com/despriv';
   // https://dev.des.metlife.com/despriv -> DES intermedio
   // http://usazebasu0350l.met_intnet.net:35741/cp-desws-priv -> DES directo
 
-  public static readonly URL_SERVICE_DEV: string = 'https://dev.des.metlife.com/despriv';
+  public static readonly URL_SERVICE_DEV: string = 'https://int.des.metlife.com/despriv';
 }
 
 
