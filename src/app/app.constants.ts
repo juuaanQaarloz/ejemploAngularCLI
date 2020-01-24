ca72f4753cec29e3586d05d4dab40b7cd208e378
Israel Esteban Gomez Lopez <israel-esteban.gomez-lopez@metlife.com.mx>
2020-01-24 06:26:12 -0600

    Se ajustan paths para capa intermedia

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -2,7 +2,7 @@
  * appConstants, aqui se definen todas las constantes que se utilizaran en la aplicación.
  */
 export class AppConstants {
-  public static readonly URL_SERVICE: string = 'https://int.des.metlife.com/despriv';
+  public static readonly URL_SERVICE: string = 'https://dev.des.metlife.com/despriv';
   // https://dev.des.metlife.com/despriv -> DES intermedio
   // http://usazebasu0350l.met_intnet.net:35741/cp-desws-priv -> DES directo
   // http://localhost:9080/despriv -> local
