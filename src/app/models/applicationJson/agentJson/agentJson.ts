7b01f1e178c9195fb0f38e306454ff6bdc3936fd
odalysmarron <odalys.marron@solinte.com.mx>
2020-01-27 17:18:52 -0600

    add crnty_id to swagger model

--- src/app/models/applicationJson/agentJson/agentJson.ts
+++ src/app/models/applicationJson/agentJson/agentJson.ts
@@ -1,15 +1,15 @@
 import {AgentCd} from './agentCd';
 
 export class AgentJson {
-  app_id?: number = 0;
+  app_id: number = 0;
   agnt_id: string = '';
-  agnt_cd?: AgentCd = new AgentCd();
-  agnt_fmt_2_id?: number = 0;
-  agnt_part_per?: number = 0;
-  part_ord?: number = 0;
-  rec_crt_ts?: string = null;
-  rec_crt_usr_id?: string = 'string';
-  rec_updt_ts?: string = null;
-  rec_updt_usr_id?: string = 'string';
+  agnt_cd: AgentCd = new AgentCd();
+  agnt_fmt_2_id: number = 0;
+  agnt_part_per: number = 0;
+  part_ord: number = 0;
+  rec_crt_ts: string = null;
+  rec_crt_usr_id: string = 'string';
+  rec_updt_ts: string = null;
+  rec_updt_usr_id: string = 'string';
 
 }
