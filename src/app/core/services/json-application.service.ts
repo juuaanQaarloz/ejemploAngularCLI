5e0848da39eb3577aa07373dd5a93d3163b83ece
Eduardo Yair Muñoz <ymunoz@grupo-ditec.com.mx>
2020-01-30 17:19:38 -0600

    Correction about label in table payment (CLABE -> TOKEN/CLABE). Correction for mapping of PDF - TypeCard

--- src/app/core/services/json-application.service.ts
+++ src/app/core/services/json-application.service.ts
@@ -264,7 +264,7 @@ export class JsonApplicationService {
           newBanckAccount.std_bnk_cd = item.txtClabe;
         } else if (item.txtClabe.length === 16){
           newBanckAccount.bnk_acct_tkn_num = item.txtClabe;
-          newAccount.clct_card_typ_id = item.selectCard;
+          this.appJson.accounts[0].clct_card_typ_id = item.selectCard;
         } else {
           newBanckAccount.bnk_acct_tkn_num = item.txtClabe;
         }
@@ -278,7 +278,7 @@ export class JsonApplicationService {
         newAccount.prfr_cntct_typ_nm = this.accountZero.prfr_cntct_typ_nm;
         if (item.txtClabe.length === 18) {
           newBanckAccount.std_bnk_cd = item.txtClabe;
-        } else if (item.txtClabe.length === 16){
+        } else if (item.txtClabe.length === 16) {
           newBanckAccount.bnk_acct_tkn_num = item.txtClabe;
           newAccount.clct_card_typ_id = item.selectCard;
         } else {
